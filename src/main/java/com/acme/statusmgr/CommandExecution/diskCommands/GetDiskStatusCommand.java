d9e8b68b96b37a9e686728620ed52e3ab39fb327
Moshe <guitaristmo@gmail.com>
2019-12-15 14:24:25 -0800

    Cleanup GetDiskStatusCommand no longer keeps track of the name and id..., since it creates the DiskStatus immediately.

--- src/main/java/com/acme/statusmgr/CommandExecution/diskCommands/GetDiskStatusCommand.java
+++ src/main/java/com/acme/statusmgr/CommandExecution/diskCommands/GetDiskStatusCommand.java
@@ -9,15 +9,9 @@ import com.acme.statusmgr.beans.DiskStatus;
 public class GetDiskStatusCommand implements IDiskStatusCommand
 {
     private DiskStatus result;
-    private long id;
-    private String template;
-    private String name;
 
     public GetDiskStatusCommand(long id, String template, String name)
     {
-        this.id = id;
-        this.template = template;
-        this.name = name;
         result = new DiskStatus(id, String.format(template, name));
     }
 
